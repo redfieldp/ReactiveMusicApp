9f340c7bdc4b3d522f32a981162dea9ce52a8ccc
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-05-24 16:29:05 -0400

    Defined a user app, and the hooks for the abstract reactive functionality.

---
+++ src/reactivemusicapp/MyMusicApp.java
@@ -0,0 +1,24 @@
+package reactivemusicapp;
+
+public class MyMusicApp extends ReactiveMusicApp
+{
+
+    protected void setupImpl()
+    {
+        // Your setup work should go here
+
+    }
+
+    protected void drawImpl()
+    {
+        // Your draw work should go here
+
+    }
+
+    protected void keyPressedImpl()
+    {
+        // Your key controls should go here
+        
+    }
+
+}
