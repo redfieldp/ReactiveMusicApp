c985f033019acb7fdfa9fda979cbef398691777a
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-05-24 17:50:31 -0400

    Functional drawing of buffer values (needs to be changed to FFT)

--- src/reactivemusicapp/MyMusicApp.java
+++ src/reactivemusicapp/MyMusicApp.java
@@ -6,13 +6,13 @@ public class MyMusicApp extends ReactiveMusicApp
     protected void setupImpl()
     {
         // Your setup work should go here
-
+        size(1024,768);
     }
 
     protected void drawImpl()
     {
         // Your draw work should go here
-
+        background(255);
     }
 
     protected void keyPressedImpl()
--- src/reactivemusicapp/ReactiveMusicApp.java
+++ src/reactivemusicapp/ReactiveMusicApp.java
@@ -33,11 +33,39 @@ public abstract class ReactiveMusicApp extends PApplet {
         drawImpl();
         
         // Once the user's drawing is done, we draw over the visuals if they are active
-        
+        if(drawInterface) {
+            int bandRangeCounter = 0;
+            for(int i = 0; i < userInput.bufferSize() - 1; i++)
+            {
+              stroke(0, 128);
+              
+              // Scale some data for drawing purposes
+              int xCoord = (int)map(i, 0, userInput.bufferSize() - 1, 0, width);
+              int scaledHeightLeft = (int)map(userInput.left.get(i), -1, 1, 0, height/2);
+              int scaledHeightRight = (int)map(userInput.right.get(i), -1, 1, 0, height/2);
+              
+              // For each channel draw a line of current levels so we can see where the 
+              // waveforms fall in the selected bands
+              line( xCoord, height/2, xCoord, (height/2) - scaledHeightLeft);
+              line( xCoord, height, xCoord, height - scaledHeightRight);
+              
+              // If one of the band ranges stops here, draw an indicator line
+              if (bandRanges[bandRangeCounter] == i) {
+                  if (bandRangeCounter < bandRanges.length - 1) {
+                      bandRangeCounter++;
+                  }
+                  stroke(255,0,0);
+                  line(xCoord, 0, xCoord, height);
+              }
+            }
+        }
     }
     
     public void keyPressed() {
         // All the controls for the audio reactivity
+        if (key == ' ') {
+            drawInterface = !drawInterface;
+        }
         
         // Call the user's key controls
         keyPressedImpl();
@@ -51,6 +79,7 @@ public abstract class ReactiveMusicApp extends PApplet {
     public void setBandCount(int myBandCount) {
         bandRanges = new int[myBandCount];
         actualBandLevels = new float[myBandCount];
+        scaledBandLevels = new float[myBandCount];
         bandMaxes = new float[myBandCount];
         for (int i = 0; i < myBandCount; i++) {
             bandRanges[i] = (i + 1) * (userInput.bufferSize()/myBandCount);
