2f6e58de211b9cf981f9ddb7e626f677071fba26
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-05-25 16:26:32 -0400

    Adjustable FFT range and band count.

--- src/reactivemusicapp/ReactiveMusicApp.java
+++ src/reactivemusicapp/ReactiveMusicApp.java
@@ -15,104 +15,156 @@ public abstract class ReactiveMusicApp extends PApplet {
     float[] actualBandLevels;
     float[] scaledBandLevels;
     float[] bandMaxes;
-    
-    boolean drawInterface = false;
+    int fftCap;
+
+    boolean active = false;
 
     public void setup() {
         // Setup variables for the reactive audio hooks
         minim = new Minim(this);
         userInput = minim.getLineIn(Minim.STEREO, 4096, 44100);
-        
+
         leftFFT = new FFT(userInput.left.size(), 44100);
         rightFFT = new FFT(userInput.right.size(), 44100);
+
+        // Starting with the lower 1/4 of the FFT usually covers most music output
+        fftCap = leftFFT.specSize()/15;
         
-        bandCount = 5;
-        setBandCount(bandCount);
-        
+        setBandCount(5);
+
         // After we have setup our variables, call the user's setup
         setupImpl();
     }
-    
+
     public void draw() {
         updateAudioData();
-        
+
         drawImpl();
-        
+
         // Once the user's drawing is done, we draw over the visuals if they are active
-        if(drawInterface) {
+        if(active) {
             int bandRangeCounter = 0;
-            for(int i = 0; i < leftFFT.specSize() - 1; i++)
+            for(int i = 0; i < fftCap - 1; i++)
             {
-              stroke(0, 128);
-              
-              // Scale some data for drawing purposes
-              int xCoord = (int)map(i, 0, leftFFT.specSize() - 1, 0, width);
-              int scaledHeightLeft = (int)leftFFT.getBand(i);
-              int scaledHeightRight = (int)rightFFT.getBand(i);
-              
-              // For each channel draw a line of current levels so we can see where the 
-              // waveforms fall in the selected bands
-              line( xCoord, height/2, xCoord, (height/2) - scaledHeightLeft);
-              line( xCoord, height, xCoord, height - scaledHeightRight);
-              
-              // If one of the band ranges stops here, draw an indicator line
-              if (bandRanges[bandRangeCounter] == i) {
-                  if (bandRangeCounter < bandRanges.length - 1) {
-                      bandRangeCounter++;
-                  }
-                  stroke(255,0,0);
-                  line(xCoord, 0, xCoord, height);
-              }
+                stroke(0, 128);
+
+                // Scale some data for drawing purposes
+                int xCoord = (int)map(i, 0, fftCap - 1, 0, width);
+                int scaledHeightLeft = (int)leftFFT.getBand(i);
+                int scaledHeightRight = (int)rightFFT.getBand(i);
+
+                // For each channel draw a line of current levels so we can see where the 
+                // waveforms fall in the selected bands
+                line( xCoord, height/2, xCoord, (height/2) - scaledHeightLeft);
+                line( xCoord, height, xCoord, height - scaledHeightRight);
+
+                // If one of the band ranges stops here, draw an indicator line
+                if (bandRanges[bandRangeCounter] == i) {
+                    if (bandRangeCounter < bandRanges.length - 1) {
+                        bandRangeCounter++;
+                    }
+                    stroke(255,0,0);
+                    line(xCoord, height, xCoord, 0);
+                }
             }
         }
     }
-    
+
     public void keyPressed() {
-        // All the controls for the audio reactivity
         if (key == ' ') {
-            drawInterface = !drawInterface;
+            active = !active;
         }
-        
-        // Call the user's key controls
-        keyPressedImpl();
+
+        // All the controls for the audio reactivity, only valid if we are active
+        if (active) {
+            if (key == CODED) {
+                if (keyCode == UP && fftCap < leftFFT.specSize()) {
+                    setFFTCap(fftCap + 1);
+                }
+                else if (keyCode == DOWN && fftCap > bandCount){
+                    setFFTCap(fftCap - 1);
+                }
+                else if (keyCode == LEFT && bandCount > 1) {
+                    setBandCount(bandCount-1);
+                }
+                else if (keyCode == RIGHT && bandCount < 10) {
+                    setBandCount(bandCount+1);
+                }
+            }
+        }
+        else {
+            // Else call the user's key controls
+            keyPressedImpl();
+        }
     }
-    
+
     /**
      * Sets the number of bands and reinitializes all of the band variables accordingly
      * 
      * @param myBandCount the number of volume bands the user desires
      */
     public void setBandCount(int myBandCount) {
+        bandCount = myBandCount;
         bandRanges = new int[myBandCount];
         actualBandLevels = new float[myBandCount];
         scaledBandLevels = new float[myBandCount];
         bandMaxes = new float[myBandCount];
         for (int i = 0; i < myBandCount; i++) {
-            bandRanges[i] = (i + 1) * (leftFFT.specSize()/myBandCount);
+            bandRanges[i] = (i + 1) * (fftCap/myBandCount);
             actualBandLevels[i] = 0;
             scaledBandLevels[i] = 0;
             bandMaxes[i] = 0;
         }
     }
     
+    public void setFFTCap(int newCap) {
+        fftCap = newCap;
+        setBandCount(bandCount);
+    }
+
     /**
      * Update the audio data based on current number of bands and the active input
      */
     public void updateAudioData() {
+        // Push FFT
         leftFFT.forward(userInput.left);
         rightFFT.forward(userInput.right);
+        
+        // Add the bands in each range to the level counters
+        int bandRangeCounter = 0;
+        for (int i = 0; i < leftFFT.specSize(); i++) {
+            actualBandLevels[bandRangeCounter] += leftFFT.getBand(i);
+            actualBandLevels[bandRangeCounter] += rightFFT.getBand(i);
+            
+            // When reaching in the end of the 
+            if (bandRanges[bandRangeCounter] == i) {
+                // If we surpassed the previous max for this range, store the max
+                if (actualBandLevels[bandRangeCounter] > bandMaxes[bandRangeCounter]) {
+                    bandMaxes[bandRangeCounter] = actualBandLevels[bandRangeCounter];
+                }
+                
+                if (bandRangeCounter < bandRanges.length - 1) {
+                    // Move to the next range segment if there is one
+                    bandRangeCounter++;
+                }
+                else {
+                    // We don't need to store data from any of the FFT range above our max
+                    break;
+                }
+            }
+        }
     }
-    
+
     /**
      * User's setup actions
      */
     protected abstract void setupImpl();
-    
+
     /**
      * User's draw actions
      */
     protected abstract void drawImpl();
-    
+
     /**
      * User's key controls
      */
