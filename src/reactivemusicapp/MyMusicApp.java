105f46f2297cfc540bd9ae4dfd16415ceb8798cd
Patrick Proctor <patrick.proctor@codeandtheory.com>
2014-05-27 18:44:16 -0400

    Added key presses, modes, and blackout to the example app.

--- src/reactivemusicapp/MyMusicApp.java
+++ src/reactivemusicapp/MyMusicApp.java
@@ -2,7 +2,8 @@ package reactivemusicapp;
 
 public class MyMusicApp extends ReactiveMusicApp
 {
-
+    int mode = 0;
+    
     protected void setupImpl()
     {
         // Your setup work should go here
@@ -13,12 +14,72 @@ public class MyMusicApp extends ReactiveMusicApp
     {
         // Your draw work should go here
         background(255);
+        if (mode == 0) {
+            fill(0);
+        }
+        else if (mode == 1) {
+            
+        }
+        else if (mode == 2) {
+            
+        }
+        else if (mode == 3) {
+            
+        }
+        else if (mode == 4) {
+            
+        }
+        else if (mode == 5) {
+            
+        }
+        else if (mode == 6) {
+            
+        }
+        else if (mode == 7) {
+            
+        }
+        else if (mode == 8) {
+            
+        }
+        else if (mode == 9) {
+            
+        }
     }
 
     protected void keyPressedImpl()
     {
         // Your key controls should go here
-        
+        if (key == '1' ){
+            mode = 1;
+        }
+        else if (key == '2') {
+            mode = 2;
+        }
+        else if (key == '3') {
+            mode = 3;
+        }
+        else if (key == '4') {
+            mode = 4;
+        }
+        else if (key == '5') {
+            mode = 5;
+        }
+        else if (key == '6') {
+            mode = 6;
+        }
+        else if (key == '7') {
+            mode = 7;
+        }
+        else if (key == '8') {
+            mode = 8;
+        }
+        else if (key == '9') {
+            mode = 9;
+        }
+        else if (key == '0') {
+            // mode 0 is blackout
+            mode = 0;
+        }
     }
 
 }
